2ae2029c6adf86284f0aac9d471ad4912d9af547
Samuel Daza <samueldazadaza@gmail.com>
2021-02-15 16:47:05 -0500

    nuevas variables es6

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,4 +1,4 @@
-//  de es6
+//CLASE2 -- antes de es6
 
 function newFunction(name, AbstractRange, edad) {
     var name = name || 'oscar';
@@ -23,3 +23,34 @@ console.log(epicphrase);
 //union ahora es6
 let epicphrase2 = `${hello} ${world} 22`;
 console.log(epicphrase2);
+
+// CLASE3 //-- variable string LET y CONST
+let lorem = "primera frase super epica de celismx \n" +
+"otra(segunda) frase epica que necesitamos."
+
+let lorem2 = `tercera frase que necesitamos
+ahora es la cuarta frase epica`;
+
+console.log(lorem);
+console.log(lorem2)
+
+// destructuración
+let person = {
+    'name' : 'oscar' ,
+    'age' : 32,
+    'country' : 'MX',
+}
+console.log(person.name, person.age);
+
+//despues con es6
+let { name, age } = person;
+console.log(name, age);
+
+//operador de propagación
+
+let team1 = ['oscar', 'julian', 'ricardo'];
+let team2 = ['valeria', 'yesica', 'camila'];
+
+let education = ['david', ...team1, ...team2];
+
+console.log(education);
